92bfa77ed9e767de5cb7c50212d6529964f507c2
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2024-11-04 00:16:56 +0800

    complete exercise 7.10 by using redux

---
+++ bloglist-frontend-redux/src/components/Notification.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux'
+
+const Notification = () => {
+  const message = useSelector(state => state.notification)
+  if (message === null) {
+    return null
+  }
+  if (message.type === 'error') {
+    return <div className="error">{message.text}</div>
+  }
+
+  if (message.type === 'success') {
+    return <div className="success">{message.text}</div>
+  }
+}
+
+export default Notification
---
+++ bloglist-frontend-redux/src/reducers/notificationReducer.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: null,
+  reducers: {
+    changeNotification(state, action){
+      return action.payload
+    },
+    removeNotification(state, action){
+      return null
+    }
+  }
+})
+
+export const { changeNotification, removeNotification } = notificationSlice.actions
+export const showNotification = (message, displayTime=5) => {
+  return (dispatch) => {
+    dispatch(changeNotification(message))
+    setTimeout(() => {
+      dispatch(removeNotification())
+    }, displayTime * 1000)
+  }
+}
+export default notificationSlice.reducer
